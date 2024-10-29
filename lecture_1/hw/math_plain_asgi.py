d03769e3e620a281ed9db46b9fcb456535af65bd
Igor Prokofyev <98826743+Reveur1988@users.noreply.github.com>
2024-10-29 15:09:16 +0400

    Add homework 1 (#1)

--- lecture_1/hw/math_plain_asgi.py
+++ lecture_1/hw/math_plain_asgi.py
@@ -1,8 +1,36 @@
-from typing import Any, Awaitable, Callable
+from typing import Dict
+from lecture_1.hw.handlers import handle_factorial, handle_fibonacci, handle_mean
+from lecture_1.hw.utils import send_json_response
+
+async def app(scope: Dict[str, str], receive, send) -> None:
+    if scope['type'] == 'lifespan':
+        while True:
+            event = await receive()
+            if event['type'] == 'lifespan.startup':
+                await send({"type": "lifespan.startup.complete"})
+            elif event['type'] == 'lifespan.shutdown':
+                await send({"type": "lifespan.shutdown.complete"})
+                break
+        return
+    
+    assert scope['type'] == 'http'
+
+    method = scope['method']
+    path = scope['path']
+
+    if method == 'GET' and path == '/factorial':
+        await handle_factorial(send, scope)
+
+    elif method == 'GET' and path.startswith('/fibonacci/'):
+        await handle_fibonacci(send, path)
+
+    elif method == 'GET' and path == '/mean':
+        await handle_mean(receive, send)
+
+    else:
+        await send_json_response(send, 404, {"error": "Not Found"})
 
 
-async def app(
-    scope: dict[str, Any],
-    receive: Callable[[], Awaitable[dict[str, Any]]],
-    send: Callable[[dict[str, Any]], Awaitable[None]],
-) -> None: ...
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
